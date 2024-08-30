043f26b833c84c5803a30df351a2efadd618de6f
Kelly Liao <kellyliao@KellydeMacBook-Air.local>
2024-08-31 01:16:34 +0800

    route and route group

--- test_v8/routes/web.php
+++ test_v8/routes/web.php
@@ -17,6 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// line 24-31 is for testing how the route works 
+// route::get('/home',function(){
+//     return "home";
+// });
+
+// route::get('/about',function(){
+//     return "about page";
+// });
+
+// route::get('/about',[App\Http\Controllers\Testcontroller::class,'index']);
+// route::get('/about/{id}',[App\Http\Controllers\Testcontroller::class,'index']);
+route::get('/about-us',[App\Http\Controllers\Testcontroller::class,'index'])->name('about');
+
+// way 1
+route::get('/account/register',[App\Http\Controllers\Testcontroller::class,'register']);
+route::get('/account/login',[App\Http\Controllers\Testcontroller::class,'login']);
+
+// way 2
+route::group(['prefix'=>'account'],function(){
+    route::get('/register',[App\Http\Controllers\Testcontroller::class,'register']);
+    route::get('/login',[App\Http\Controllers\Testcontroller::class,'login']);
+    
+});
